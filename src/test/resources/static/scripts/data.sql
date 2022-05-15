2e8ac4ff4c9c46408dea35294d97320ce7f75e3e
Dmitriy Sakhno <dim4one1988@gmail.com>
2022-05-15 21:41:01 +0300

    changes for test jenkins trigger

--- src/test/resources/static/scripts/data.sql
+++ src/test/resources/static/scripts/data.sql
@@ -3,12 +3,10 @@ values ('John Malkovich'),
        ('Paul McKinley'),
        ('Christopher Robertson'),
        ('Edvard Schwarts');
-
 insert into balance(user_id, amount)
 values (1, 120),
        (2, 2000),
        (4, 0);
-
 insert into operations(user_id, type, amount, date)
 values (1, 'D', 100, '2022-01-01 10:00:00'),
        (1, 'D', 20, '2022-01-01 12:00:00'),
